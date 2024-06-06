60a84b0a96f707435754e6bd1768cd5a8629ee75
Prakhar Verma <connect@iprakharv.com>
2024-06-06 18:01:22 +0530

    Added OnBoardingState with pageIndex and onBoardingDataList for managing onboarding state

---
+++ lib/screens/welcome_onBoard/onBoard/bloc/onBoarding_state.dart
@@ -0,0 +1,8 @@
+part of 'onBoarding_bloc.dart';
+
+class OnBoardingState {
+  int pageIndex;
+  List<OnBoardingData> onBoardingDataList = OnBoardingData.onBoardingDataList;
+
+  OnBoardingState({this.pageIndex = 0});
+}
